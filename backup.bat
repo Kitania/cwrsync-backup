631948c818e0d79ffb4ae75c89c8682c1f1b80c6
Hero Solutions <info@herosolutions.be>
2020-10-18 21:05:59 +0200

    Implements cwrsync-backup, adds sample configuration

--- backup.bat
+++ backup.bat
@@ -1,5 +1,56 @@
 @echo off
+call config.bat
 
-:: TODO implement this
+echo.
+echo Please check if the following configuration is correct:
+echo.
+echo Rsync binary path:  %rsync_bin_path%
+echo Destination folder: %dst_drive%:/%dst_folder%
+echo.
+echo Folders to copy:
+echo.
+
+set "x=0"
+:checkloop
+if defined paths[%x%].src (
+
+	call echo %src_drive%:/%%paths[%x%].src%% 
+	call echo to      %dst_drive%:/%dst_folder%/%%paths[%x%].dst%%
+    call echo.
+
+    set /a "x+=1"
+
+    goto :checkloop
+)
+
+:choice
+set /P c="Is this correct [y/n]?"
+if /I "%c%" EQU "y" goto :somewhere
+if /I "%c%" EQU "n" goto :somewhere_else
+goto :choice
+
+:somewhere
+
+	set "x=0"
+	:symloop
+	if defined paths[%x%].src (
+
+		call set "dst_loc=%dst_drive%:/%dst_folder%/%%paths[%x%].dst%%"
+
+		if not exist "%dst_loc%" (
+			call mkdir "%dst_loc%"
+		)
+
+		call set "src_arg=/cygdrive/%src_drive%/%%paths[%x%].src%%"
+		call set "dst_arg=/cygdrive/%dst_drive%/%dst_folder%/%%paths[%x%].dst%%"
+
+	    call "%rsync_bin_path%" -avh "%src_arg%" "%dst_arg%"
+
+	    set /a "x+=1"
+
+	    goto :symloop
+	)
+
+:somewhere_else
 
 pause
--- config.bat
+++ config.bat
@@ -1,3 +1,14 @@
-@echo off
+:: Sample configuration, uncomment and change/add as needed
+::set "rsync_bin_path=C:/Program Files/rsync/bin/rsync.exe"
 
-:: TODO implement this
+::set "src_drive=C"
+::set "dst_drive=D"
+::set "dst_folder=Backups"
+
+:::: Note: src/dst dirs must contain a trailing slash!
+
+::set "paths[0].src=Program Files/7-Zip/"
+::set "paths[0].dst=backup_20201018/Program Files/7-Zip/"
+
+::set "paths[1].src=Program Files/Git/"
+::set "paths[1].dst=backup_20201018/Program Files/Git/"
